5a88a80ed0fe0f46ffc53e28f3b8d700982f0585
Alexandre Henrique <alexandrehenrique.es@gmail.com>
2022-01-21 17:14:26 -0400

    ✨ NEW FEATURE: Adicionado rota para verificar o status da API

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,7 +3,10 @@ const companyRouters = require('./company')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('App online!')
+    res.status(200).send({
+        status: 200,
+        message: 'API is online!',
+    })
 })
 
 router.use('/company', companyRouters)
